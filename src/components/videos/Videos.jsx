5a1de41286a3d8efff248be6d6385167867b7071
im-azad <me.azad99@gmail.com>
2024-02-11 23:26:41 +0600

    videos get

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,15 +1,21 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+    const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+    // decide what to render
+    let content = null;
+
+    if (isLoading) content = <VideoLoader />;
+    if (!isLoading && isError) content = <Error />;
+    if (!isLoading && !isError && videos?.length === 0)
+        content = <div>No videos found</div>;
+    if (!isLoading && !isError && videos?.length > 0) {
+        content = videos.map((video) => <Video key={video.id} {...video} />);
+    }
+
+    return content;
 }
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,12 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://localhost:9000",
+        baseUrl: "http://localhost:9000",
     }),
-	endpoints: (builder) => ({
-		getVideos: builder.query({
-			query: () => "/videos",
-		})
-	})
+    endpoints: (builder) => ({
+        getVideos: builder.query({
+            query: () => "/videos",
+        }),
+    }),
 });
- 
+
+export const { useGetVideosQuery } = apiSlice;
